ae2a79bfd8533bebf8a5cc204c3ec54563e55fca
Thomas Cheyney <top_cat@thomasc.co.uk>
2024-08-15 12:50:22 +0100

    Add required staging schema

---
+++ src/service/service-directory-api/src/FamilyHubs.ServiceDirectory.Data/Entities/Staging/ServicesTemp.cs
@@ -0,0 +1,8 @@
+﻿namespace FamilyHubs.ServiceDirectory.Data.Entities.Staging;
+
+public class ServicesTemp
+{
+    public long Id { get; set; }
+    public required string Json { get; set; }
+    public required DateTime LastModified { get; set; }
+}
