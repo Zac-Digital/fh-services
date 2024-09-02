b0b402bf978f554cc0049442285c9db62ad64209
Zac <119302767+Zac-AND-Digital@users.noreply.github.com>
2024-09-02 10:55:41 +0100

    FHB-87 : Move Component to Shared Kernel (#74)
    * feat: add ServicesTemp to Shared Kernel
    
    * chore: bump version

---
+++ src/shared/shared-kernel/src/fh-shared-kernel.shared-kernel/OpenReferral/ServicesTemp.cs
@@ -0,0 +1,8 @@
+namespace FamilyHubs.SharedKernel.OpenReferral;
+
+public class ServicesTemp
+{
+    public Guid Id { get; set; }
+    public required string Json { get; set; }
+    public required DateTime LastModified { get; set; }
+}
