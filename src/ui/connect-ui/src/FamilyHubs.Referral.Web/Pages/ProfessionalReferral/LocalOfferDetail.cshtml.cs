a8f4dfc1e6ed022ba264b3f1a19ed44a0c86b5e0
Phil Davies <github@phildavies.co.uk>
2024-09-05 10:24:32 +0100

    show service schedules

--- src/ui/connect-ui/src/FamilyHubs.Referral.Web/Pages/ProfessionalReferral/LocalOfferDetail.cshtml.cs
+++ src/ui/connect-ui/src/FamilyHubs.Referral.Web/Pages/ProfessionalReferral/LocalOfferDetail.cshtml.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Primitives;
 using Microsoft.AspNetCore.Authorization;
 using FamilyHubs.Referral.Web.Pages.Shared;
 using FamilyHubs.SharedKernel.Identity;
+using FamilyHubs.ServiceDirectory.Shared.Enums;
 
 namespace FamilyHubs.Referral.Web.Pages.ProfessionalReferral;
 
@@ -14,6 +15,8 @@ public class LocalOfferDetailModel : HeaderPageModel
     private readonly IOrganisationClientService _organisationClientService;
     private readonly IIdamsClient _idamsClient;
     public ServiceDto LocalOffer { get; set; } = default!;
+    public List<AttendingType> ServiceScheduleAttendingTypes { get; set; } = default!;
+    public ScheduleDto? ServiceSchedule { get; set; }
 
     public string? ReturnUrl { get; set; }
 
@@ -37,9 +40,66 @@ public class LocalOfferDetailModel : HeaderPageModel
         ReturnUrl = StringValues.IsNullOrEmpty(referer) ? Url.Page("Search") : referer.ToString();
         LocalOffer = await _organisationClientService.GetLocalOfferById(serviceId);
 
+        (ServiceScheduleAttendingTypes, ServiceSchedule) = GetServiceSchedule();
+
         ShowConnectionRequestButton = await ShouldShowConnectionRequestButton();
 
         return Page();
+    }
+
+    // this only covers scenarios that can be created through Manage, not all possible scenarios from LA ingested data
+
+    //private (List<string>, IEnumerable<string>?) GetServiceSchedule()
+    //private (List<string>, ScheduleDto?) GetServiceSchedule()
+    private (List<AttendingType>, ScheduleDto?) GetServiceSchedule()
+    {
+        //var serviceScheduleAttendingTypeDescriptions = new List<string>();
+        var serviceScheduleAttendingTypes = new List<AttendingType>();
+
+        //var firstServiceSchedule = LocalOffer.Schedules.FirstOrDefault();
+
+        //if (firstServiceSchedule == null)
+        //{
+        //    return (serviceScheduleAttendingTypeDescriptions, null);
+        //}
+
+        foreach (var attendingTypeString in LocalOffer.Schedules
+                     .Select(s => s.AttendingType))
+        {
+            if (attendingTypeString != null
+                && Enum.TryParse<AttendingType>(attendingTypeString, out var attendingType)
+                && (attendingType != AttendingType.InPerson
+                    || !LocalOffer.Locations.Any()))
+            {
+                //string desc = attendingType.ToDescription();
+                //serviceScheduleAttendingTypeDescriptions.Add(desc[0] + desc[1..].ToLower());
+                serviceScheduleAttendingTypes.Add(attendingType);
+            }
+        }
+
+        if (!serviceScheduleAttendingTypes.Any())
+        {
+            return (serviceScheduleAttendingTypes, null);
+        }
+
+        return (serviceScheduleAttendingTypes, LocalOffer.Schedules.FirstOrDefault());
+
+        //var firstServiceSchedule = LocalOffer.Schedules.FirstOrDefault();
+
+        //return (serviceScheduleAttendingTypeDescriptions,
+        //    firstServiceSchedule!.ByDay?.Split(",")
+        //                       ?? Enumerable.Empty<string>()
+        //        .Select(c => Calendar.DayCodeToName[c]));
+
+        //return firstServiceSchedule;
+
+        //if (LocalOffer.Locations.Any())
+        //    //|| LocalOffer.ServiceDeliveries?.All(sd => sd.Name != AttendingType.InPerson) == true)
+        //{
+        //    return null;
+        //}
+        //return LocalOffer.Schedules
+        //    .FirstOrDefault(s => s.AttendingType == AttendingType.InPerson.ToString());
     }
 
     private async Task<bool> ShouldShowConnectionRequestButton()
