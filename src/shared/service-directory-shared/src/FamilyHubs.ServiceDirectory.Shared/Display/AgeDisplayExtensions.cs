5337143eecee3e89d02e70906fc31fe2b45df5d8
Thomas Cheyney <top_cat@thomasc.co.uk>
2024-10-01 11:12:10 +0100

    Move AgeToString to shared code

---
+++ src/shared/service-directory-shared/src/FamilyHubs.ServiceDirectory.Shared/Display/AgeDisplayExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace FamilyHubs.ServiceDirectory.Shared.Display;
+
+public static class AgeDisplayExtensions
+{
+    public static string AgeToString(int age)
+    {
+        return age == 127 ? "25+" : age.ToString();
+    }
+}
