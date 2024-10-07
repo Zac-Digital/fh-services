063c06f80431f08056871e3d3a67dfa1d09de40b
Stuart Maskell <stuart.maskell@hippodigital.co.uk>
2024-10-07 16:22:36 +0100

    Fhb 736 manage UI (#160)
    * feat: Add NSubstitute and update test (squashed)
    
    * feat: remove last bits of Moq from web admin
    
    * feat: fix delete test
    
    ---------
    
    Co-authored-by: Stuart.Maskell <stuart.maskell@education.gov.uk>

--- src/ui/manage-ui/Test/FamilyHubs.ServiceDirectory.Admin.Web.UnitTests/TestHelper.cs
+++ src/ui/manage-ui/Test/FamilyHubs.ServiceDirectory.Admin.Web.UnitTests/TestHelper.cs
@@ -1,29 +1,32 @@
 ﻿using AutoFixture;
 using FamilyHubs.ServiceDirectory.Shared.Dto;
 using FamilyHubs.ServiceDirectory.Shared.Enums;
-using FamilyHubs.SharedKernel.Identity;
 using Microsoft.AspNetCore.Http;
-using Moq;
-using System.Collections.Generic;
 using System.Security.Claims;
+using NSubstitute;
 
 namespace FamilyHubs.ServiceDirectory.Admin.Web.UnitTests
 {
     internal static class TestHelper
     {
-        public static Mock<HttpContext> GetHttpContext(List<Claim> claims)
+        /// <summary>
+        /// Uses NSubstitute to create a HttpContext with a user with the given claims
+        /// </summary>
+        /// <param name="claims"></param>
+        /// <returns></returns>
+        public static HttpContext GetHttpContext(List<Claim> claims)
         {
-            var mockUser = new Mock<ClaimsPrincipal>();
-            mockUser.SetupGet(x=>x.Claims).Returns(claims);
+            var mockUser = Substitute.For<ClaimsPrincipal>();
+            mockUser.Claims.Returns(claims);
 
-            var mockCookies = new Mock<IResponseCookies>();
+            var mockCookies = Substitute.For<IResponseCookies>();
 
-            var mockResponse = new Mock<HttpResponse>();
-            mockResponse.SetupGet(x=>x.Cookies).Returns(mockCookies.Object);
+            var mockResponse = Substitute.For<HttpResponse>();
+            mockResponse.Cookies.Returns(mockCookies);
 
-            var mock = new Mock<HttpContext>();
-            mock.SetupGet(x => x.User).Returns(mockUser.Object);
-            mock.SetupGet(x=>x.Response).Returns(mockResponse.Object);
+            var mock = Substitute.For<HttpContext>();
+            mock.User.Returns(mockUser);
+            mock.Response.Returns(mockResponse);
 
             return mock;
         }
@@ -44,6 +47,26 @@ namespace FamilyHubs.ServiceDirectory.Admin.Web.UnitTests
             organisation.AssociatedOrganisationId = parentId;
             organisation.OrganisationType = organisationType;
             return organisation;
+        }
+        
+        /// <summary>
+        /// Uses NSubstitute to capture the argument passed to a method
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        public class ArgumentCaptor<T>
+        {
+            public T Capture()
+            {
+                return Arg.Is<T>(t => SaveValue(t));
+            }
+
+            private bool SaveValue(T t)
+            {
+                Value = t;
+                return true;
+            }
+
+            public T? Value { get; private set; }
         }
     }
 }
