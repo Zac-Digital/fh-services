7ccc04e8a36fdfe04840fadf3e18f5f019763681
Steve Saunders <steve.saunders@education.gov.uk>
2024-10-09 10:43:29 +0100

    FHB-761: Fix tests to use NSubstitute

---
+++ src/shared/shared-kernel/tests/fh-shared-kernel.unit-tests/Identity/TestHelpers/TestHttpMessageHandler.cs
@@ -0,0 +1,31 @@
+﻿namespace FamilyHubs.SharedKernel.UnitTests.Identity.TestHelpers;
+
+public sealed class TestHttpMessageHandler : HttpMessageHandler
+{
+    private readonly HttpResponseMessage _response;
+    private readonly Uri _baseUrl;
+    private readonly HttpMethod _httpMethod;
+
+    public TestHttpMessageHandler(HttpResponseMessage response, Uri baseUrl, HttpMethod httpMethod)
+    {
+        _response = response;
+        _baseUrl = baseUrl;
+        _httpMethod = httpMethod;
+    }
+        
+    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        Assert.True(ValidateMessage(request, _baseUrl, _httpMethod), "Test delegating handler not matched.");
+
+        return Task.FromResult(_response);
+    }
+        
+    private static bool ValidateMessage(HttpRequestMessage requestMessage, Uri baseUrl, HttpMethod httpMethod)
+    {
+        if (requestMessage.RequestUri is null) return false;
+        if (requestMessage.RequestUri != baseUrl) return false;
+        if (requestMessage.Method != httpMethod) return false;
+
+        return true;
+    }
+}
--- src/shared/shared-kernel/tests/fh-shared-kernel.unit-tests/Services/Postcodesio/PostcodeLookupTest.cs
+++ src/shared/shared-kernel/tests/fh-shared-kernel.unit-tests/Services/Postcodesio/PostcodeLookupTest.cs
@@ -1,108 +1,141 @@
-﻿using System;
-using System.Net;
-using System.Net.Http;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Net;
+using System.Text.Json;
 using FamilyHubs.SharedKernel.Services.Postcode.Model;
 using FamilyHubs.SharedKernel.Services.PostcodesIo;
-using Moq;
-using Moq.Protected;
+using FamilyHubs.SharedKernel.UnitTests.Identity.TestHelpers;
+using NSubstitute;
 
-namespace FamilyHubs.SharedKernel.UnitTests.Services.Postcodesio
+namespace FamilyHubs.SharedKernel.UnitTests.Services.Postcodesio;
+
+public class PostcodeLookupTest
 {
-    public class PostcodeLookupTest
+    private const string NotFoundResponse = "{\"status\":404,\"error\":\"Invalid postcode\"}";
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData("\t")]
+    [InlineData(" \t \t")]
+    public async Task Get_NullOrWhitespacePostcode_ReturnsNoPostcode(string? postcode)
     {
-        public Mock<IHttpClientFactory> HttpClientFactory { get; set; }
-        public PostcodesIoLookup PostcodesIoLookup { get; set; }
+        var postcodeLookup = CreatePostcodesIoLookup(postcode);
+        
+        var (postcodeError, _) = await postcodeLookup.Get(postcode);
 
-        public PostcodeLookupTest()
+        Assert.Equal(PostcodeError.NoPostcode, postcodeError);
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData("\t")]
+    [InlineData(" \t \t")]
+    public async Task Get_NullOrWhitespacePostcode_ReturnsNullPostcodeInfo(string? postcode)
+    {
+        var postcodeLookup = CreatePostcodesIoLookup(postcode);
+        
+        var (_, postcodeInfo) = await postcodeLookup.Get(postcode);
+
+        Assert.Null(postcodeInfo);
+    }
+
+    [Theory]
+    [InlineData("X")]
+    [InlineData("not a postcode")]
+    public async Task Get_InvalidPostcode_ReturnsInvalidPostcode(string? postcode)
+    {
+        var postcodeLookup = CreatePostcodesIoLookup(postcode);
+        
+        var (postcodeError, _) = await postcodeLookup.Get(postcode);
+
+        Assert.Equal(PostcodeError.InvalidPostcode, postcodeError);
+    }
+
+    [Theory]
+    [InlineData("X")]
+    [InlineData("not a postcode")]
+    public async Task Get_InvalidPostcode_ReturnsNullPostcodeInfo(string? postcode)
+    {
+        var postcodeLookup = CreatePostcodesIoLookup(postcode);
+        
+        var (_, postcodeInfo) = await postcodeLookup.Get(postcode);
+
+        Assert.Null(postcodeInfo);
+    }
+    
+    [Fact]
+    public async Task Get_NotFoundPostcode_ReturnsPostcodeNotFound()
+    {
+        var postcodeLookup = CreatePostcodesIoLookup("B11BB", SerializeUnsuccessfulLookup("Postcode not found"));
+        
+        var (postcodeError, _) = await postcodeLookup.Get("B11BB");
+
+        Assert.Equal(PostcodeError.PostcodeNotFound, postcodeError);
+    }
+    
+    [Fact]
+    public async Task Get_InvalidPostcode_ReturnsPostcodeInvalidPostcode()
+    {
+        var postcodeLookup = CreatePostcodesIoLookup("B11BB", SerializeUnsuccessfulLookup("Invalid postcode"));
+        
+        var (postcodeError, _) = await postcodeLookup.Get("B11BB");
+
+        Assert.Equal(PostcodeError.InvalidPostcode, postcodeError);
+    }
+
+    [Fact]
+    public async Task Get_NotFoundPostcode_ReturnsNullPostcodeInfo()
+    {
+        var postcodeLookup = CreatePostcodesIoLookup("B11BB");
+        
+        var (_, postcodeInfo) = await postcodeLookup.Get("B11BB");
+
+        Assert.Null(postcodeInfo);
+    }
+    
+    [Fact]
+    public async Task Get_ValidPostcode_ReturnsPostcodeInfo()
+    {
+        var postcodeLookup = CreatePostcodesIoLookup("E1 5LX", SerializeSuccessfulLookup("E1 5LX"));
+        
+        var (_, postcodeInfo) = await postcodeLookup.Get("E1 5LX");
+
+        Assert.NotNull(postcodeInfo);
+    }
+
+    private static string SerializeSuccessfulLookup(string postcode)
+    {
+        return JsonSerializer.Serialize(new PostcodesIoResponse
         {
-            HttpClientFactory = new Mock<IHttpClientFactory>();
+            PostcodeInfo = new PostcodeInfo(postcode, 51.519437f, -0.06992f, new Codes("E09000030", "E99999999"))
+        });
+    }
+    
+    private static string SerializeUnsuccessfulLookup(string error)
+    {
+        return JsonSerializer.Serialize(new PostcodesIoResponse { Error = error });
+    }
+    
+    private static PostcodesIoLookup CreatePostcodesIoLookup(string? postcode, string? resultJson = NotFoundResponse)
+    {
+        var httpClientFactory = Substitute.For<IHttpClientFactory>();
 
-            var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
-            mockHttpMessageHandler.Protected()
-                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
-                .ReturnsAsync(new HttpResponseMessage
-                {
-                    StatusCode = HttpStatusCode.NotFound,
-                    Content = new StringContent("{\"status\":404,\"error\":\"Invalid postcode\"}"),
-                });
-
-            var client = new HttpClient(mockHttpMessageHandler.Object)
-            {
-                BaseAddress = new Uri("https://example.com/")
-            };
-
-            HttpClientFactory.Setup(_ => _.CreateClient("postcodesio")).Returns(client);
-
-            PostcodesIoLookup = new PostcodesIoLookup(HttpClientFactory.Object);
-        }
-
-        [Theory]
-        [InlineData(null)]
-        [InlineData("")]
-        [InlineData(" ")]
-        [InlineData("\t")]
-        [InlineData(" \t \t")]
-        public async Task Get_NullOrWhitespacePostcode_ReturnsNoPostcode(string? postcode)
+        var response = new HttpResponseMessage
         {
-            var (postcodeError, _) = await PostcodesIoLookup.Get(postcode);
-
-            Assert.Equal(PostcodeError.NoPostcode, postcodeError);
-        }
-
-        [Theory]
-        [InlineData(null)]
-        [InlineData("")]
-        [InlineData(" ")]
-        [InlineData("\t")]
-        [InlineData(" \t \t")]
-        public async Task Get_NullOrWhitespacePostcode_ReturnsNullPostcodeInfo(string? postcode)
+            StatusCode = HttpStatusCode.NotFound,
+            Content = new StringContent(resultJson!)
+        };
+        var expectedUrl = new Uri($"https://example.com/{postcode}");
+            
+        var client = new HttpClient(new TestHttpMessageHandler(response, expectedUrl, HttpMethod.Get))
         {
-            var (_, postcodeInfo) = await PostcodesIoLookup.Get(postcode);
+            BaseAddress = new Uri("https://example.com/")
+        };
 
-            Assert.Null(postcodeInfo);
-        }
+        httpClientFactory.CreateClient("postcodesio").Returns(client);
 
-        [Theory]
-        [InlineData("X")]
-        [InlineData("not a postcode")]
-        public async Task Get_InvalidPostcode_ReturnsInvalidPostcode(string? postcode)
-        {
-            var (postcodeError, _) = await PostcodesIoLookup.Get(postcode);
-
-            Assert.Equal(PostcodeError.InvalidPostcode, postcodeError);
-        }
-
-        [Theory]
-        [InlineData("X")]
-        [InlineData("not a postcode")]
-        public async Task Get_InvalidPostcode_ReturnsNullPostcodeInfo(string? postcode)
-        {
-            var (_, postcodeInfo) = await PostcodesIoLookup.Get(postcode);
-
-            Assert.Null(postcodeInfo);
-        }
-
-        //todo: tech-debt
-        //[Fact]
-        //public async Task Get_NotFoundPostcode_ReturnsPostcodeNotFound()
-        //{
-        //    var (postcodeError, _) = await PostcodesIoLookup.Get("B11BB");
-
-        //    Assert.Equal(PostcodeError.PostcodeNotFound, postcodeError);
-        //}
-
-        [Fact]
-        public async Task Get_NotFoundPostcode_ReturnsNullPostcodeInfo()
-        {
-            var (_, postcodeInfo) = await PostcodesIoLookup.Get("B11BB");
-
-            Assert.Null(postcodeInfo);
-        }
-
-        //todo: happy path mapping tests
-        //todo: "null" response
-        //todo: non success and not found status codes
+        return new PostcodesIoLookup(httpClientFactory);
     }
 }
