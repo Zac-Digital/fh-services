9ec8c90a0385637bef2ca1e9d622c8df1297e91b
Steve Saunders <steve.saunders@education.gov.uk>
2024-10-10 13:22:24 +0100

    Merge conflict: Fix Moq test and failing test cases

--- src/service/referral-api/tests/FamilyHubs.ReferralApi.UnitTests/WhenUsingReferralCommands.cs
+++ src/service/referral-api/tests/FamilyHubs.ReferralApi.UnitTests/WhenUsingReferralCommands.cs
@@ -1,5 +1,3 @@
-using AutoMapper;
-using FamilyHubs.Referral.Core;
 using FamilyHubs.Referral.Core.ClientServices;
 using FamilyHubs.Referral.Core.Commands.CreateReferral;
 using FamilyHubs.Referral.Core.Commands.SetReferralStatus;
@@ -595,72 +593,6 @@ public class WhenUsingReferralCommands : BaseCreateDbUnitTest<CreateReferralComm
         //Assert
         result.Should().NotBeNull();
         result.Items.Count.Should().Be(1);
-    }
-
-    public static ReferralDto GetReferralDto()
-    {
-        return new ReferralDto
-        {
-            Id = 2,
-            ReasonForSupport = "Reason For Support",
-            EngageWithFamily = "Engage With Family",
-            RecipientDto = new RecipientDto
-            {
-                Id = 2,
-                Name = "Joe Blogs",
-                Email = "JoeBlog@email.com",
-                Telephone = "078123456",
-                TextPhone = "078123456",
-                AddressLine1 = "Address Line 1",
-                AddressLine2 = "Address Line 2",
-                TownOrCity = "Town or City",
-                County = "County",
-                PostCode = "B30 2TV"
-            },
-            ReferralUserAccountDto = new UserAccountDto
-            {
-                Id = 2,
-                EmailAddress = "Bob.Referrer@email.com",
-                Name = "Bob Referrer",
-                PhoneNumber = "1234567890",
-                Team = "Team",
-                UserAccountRoles = new List<UserAccountRoleDto>()
-                {
-                    new UserAccountRoleDto
-                    {
-                        UserAccount = new UserAccountDto
-                        {
-                            EmailAddress = "Bob.Referrer@email.com",
-                        },
-                        Role = new RoleDto
-                        {
-                            Name = "VcsProfessional"
-                        }
-                    }
-                },
-                ServiceUserAccounts = new List<UserAccountServiceDto>(),
-                OrganisationUserAccounts = new List<UserAccountOrganisationDto>(),
-            },
-            Status = new ReferralStatusDto
-            {
-                Id = 1,
-                Name = "New",
-                SortOrder = 0
-            },
-            ReferralServiceDto = new ReferralServiceDto
-            {
-                Id = 2,
-                Name = "Service",
-                Description = "Service Description",
-                Url = "www.service.com",
-                OrganisationDto = new OrganisationDto
-                {
-                    Id = 2,
-                    Name = "Organisation",
-                    Description = "Organisation Description",
-                }
-            }
-        };
     }
 
     [Theory]
