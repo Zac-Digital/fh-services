33ee6bc9a62946734c7608ac11d84541691d8647
Steve Saunders <steve.saunders@education.gov.uk>
2024-11-05 15:56:10 +0000

    FHB-825: Added seeding and teardown script. Disabled sql to test workflow first

---
+++ test/teardown-scripts/teardown-database-objects.sql
@@ -0,0 +1,55 @@
+begin transaction
+
+SELECT [Id]
+        ,[OpenId]
+        ,[Name]
+        ,[Email]
+        ,[PhoneNumber]
+        ,[Status]
+        ,[Created]
+        ,[CreatedBy]
+        ,[LastModified]
+        ,[LastModifiedBy]
+FROM [dbo].[Accounts]
+
+
+    /*
+-- Drop all foreign key constraints
+DECLARE @sql NVARCHAR(MAX) = N'';
+SELECT @sql += 'ALTER TABLE ' + QUOTENAME(s.name) + '.' + QUOTENAME(t.name) + ' DROP CONSTRAINT ' + QUOTENAME(f.name) + ';'
+FROM sys.foreign_keys AS f
+INNER JOIN sys.tables AS t ON f.parent_object_id = t.object_id
+INNER JOIN sys.schemas AS s ON t.schema_id = s.schema_id;
+EXEC sp_executesql @sql;
+
+-- Drop all tables
+SET @sql = N'';
+SELECT @sql += 'DROP TABLE ' + QUOTENAME(s.name) + '.' + QUOTENAME(t.name) + ';'
+FROM sys.tables AS t
+INNER JOIN sys.schemas AS s ON t.schema_id = s.schema_id;
+EXEC sp_executesql @sql;
+
+-- Drop all views
+SET @sql = N'';
+SELECT @sql += 'DROP VIEW ' + QUOTENAME(s.name) + '.' + QUOTENAME(v.name) + ';'
+FROM sys.views AS v
+INNER JOIN sys.schemas AS s ON v.schema_id = s.schema_id;
+EXEC sp_executesql @sql;
+
+-- Drop all stored procedures
+SET @sql = N'';
+SELECT @sql += 'DROP PROCEDURE ' + QUOTENAME(s.name) + '.' + QUOTENAME(p.name) + ';'
+FROM sys.procedures AS p
+INNER JOIN sys.schemas AS s ON p.schema_id = s.schema_id;
+EXEC sp_executesql @sql;
+
+-- Drop all functions
+SET @sql = N'';
+SELECT @sql += 'DROP FUNCTION ' + QUOTENAME(s.name) + '.' + QUOTENAME(f.name) + ';'
+FROM sys.objects AS f
+INNER JOIN sys.schemas AS s ON f.schema_id = s.schema_id
+WHERE f.type IN ('FN', 'IF', 'TF');
+EXEC sp_executesql @sql;
+     */
+
+rollback transaction
