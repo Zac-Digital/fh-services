5e1fb9696af78d1c1c2bd8ce635a52f7713129d3
Thomas Cheyney <top_cat@thomasc.co.uk>
2024-11-05 11:22:49 +0000

    Remove flaky test

--- src/ui/find-ui/tests/FamilyHubs.ServiceDirectory.UnitTests/Web/ServiceDetailWebTests.cs
+++ src/ui/find-ui/tests/FamilyHubs.ServiceDirectory.UnitTests/Web/ServiceDetailWebTests.cs
@@ -1,11 +1,8 @@
-﻿using System.Net;
-using AngleSharp.Dom;
+﻿using AngleSharp.Dom;
 using AngleSharp.Html.Dom;
 using FamilyHubs.ServiceDirectory.Core.ServiceDirectory.Interfaces;
-using FamilyHubs.ServiceDirectory.Infrastructure.Services.ServiceDirectory;
 using Microsoft.Extensions.DependencyInjection;
 using NSubstitute;
-using NSubstitute.ExceptionExtensions;
 
 namespace FamilyHubs.ServiceDirectory.UnitTests.Web;
 
@@ -37,24 +34,5 @@ public class ServiceDetailWebTests : BaseWebTest
         var heading = page.QuerySelector("h1");
         Assert.IsAssignableFrom<IHtmlHeadingElement>(heading);
         Assert.Equal(TestData.ExampleServices[0].Name, heading.GetInnerText());
-    }
-
-    [Fact]
-    public async Task ServiceDetail_NoResults()
-    {
-        _serviceDirectoryClient.GetServiceById(Arg.Any<long>(), Arg.Any<CancellationToken>())
-            .ThrowsAsync(new ServiceDirectoryClientException(new HttpResponseMessage
-            {
-                RequestMessage = Substitute.For<HttpRequestMessage>(),
-                StatusCode = HttpStatusCode.NotFound,
-                Content = new StringContent("Content")
-            }, "Service not found"));
-
-        // Act
-        var page = await Navigate("/ServiceDetail?serviceId=-1&fromUrl=%2FServiceFilter%3Fpostcode%3DW1A%201AA%26adminarea%3DE09000033%26latitude%3D51.518562%26longitude%3D-0.143799%26frompostcodesearch%3Dtrue");
-
-        // Assert
-        var backLink = page.QuerySelector(".govuk-back-link");
-        Assert.Null(backLink);
     }
 }
