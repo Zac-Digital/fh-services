c2e0f13c66febe933f1833d72f772e2d6902a701
Steve Saunders <steve.saunders@education.gov.uk>
2024-11-06 16:00:49 +0000

    Need connect dash UI

--- test/teardown-scripts/teardown-database-objects.sql
+++ test/teardown-scripts/teardown-database-objects.sql
@@ -15,28 +15,4 @@ FROM sys.tables AS t
 INNER JOIN sys.schemas AS s ON t.schema_id = s.schema_id;
 EXEC sp_executesql @sql;
 
-/*
--- Drop all views
-SET @sql = N'';
-SELECT @sql += 'DROP VIEW ' + QUOTENAME(s.name) + '.' + QUOTENAME(v.name) + ';'
-FROM sys.views AS v
-INNER JOIN sys.schemas AS s ON v.schema_id = s.schema_id;
-EXEC sp_executesql @sql;
-
--- Drop all stored procedures
-SET @sql = N'';
-SELECT @sql += 'DROP PROCEDURE ' + QUOTENAME(s.name) + '.' + QUOTENAME(p.name) + ';'
-FROM sys.procedures AS p
-INNER JOIN sys.schemas AS s ON p.schema_id = s.schema_id;
-EXEC sp_executesql @sql;
-
--- Drop all functions
-SET @sql = N'';
-SELECT @sql += 'DROP FUNCTION ' + QUOTENAME(s.name) + '.' + QUOTENAME(f.name) + ';'
-FROM sys.objects AS f
-INNER JOIN sys.schemas AS s ON f.schema_id = s.schema_id
-WHERE f.type IN ('FN', 'IF', 'TF');
-EXEC sp_executesql @sql;
-*/
-
 COMMIT TRANSACTION
