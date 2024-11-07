3bfb96114f89d4730ea0ec82bc388e3bc3347385
Steve Saunders <steve.saunders@education.gov.uk>
2024-11-07 08:45:13 +0000

    Add try/catch block to teardown script

--- test/teardown-scripts/teardown-database-objects.sql
+++ test/teardown-scripts/teardown-database-objects.sql
@@ -1,18 +1,30 @@
-BEGIN TRANSACTION
-
--- Drop all foreign key constraints
-DECLARE @sql NVARCHAR(MAX) = N'';
-SELECT @sql += 'ALTER TABLE ' + QUOTENAME(s.name) + '.' + QUOTENAME(t.name) + ' DROP CONSTRAINT ' + QUOTENAME(f.name) + ';'
-FROM sys.foreign_keys AS f
-INNER JOIN sys.tables AS t ON f.parent_object_id = t.object_id
-INNER JOIN sys.schemas AS s ON t.schema_id = s.schema_id;
-EXEC sp_executesql @sql;
-
--- Drop all tables
-SET @sql = N'';
-SELECT @sql += 'DROP TABLE ' + QUOTENAME(s.name) + '.' + QUOTENAME(t.name) + ';'
-FROM sys.tables AS t
-INNER JOIN sys.schemas AS s ON t.schema_id = s.schema_id;
-EXEC sp_executesql @sql;
-
-COMMIT TRANSACTION
+BEGIN TRY
+    BEGIN TRANSACTION
+    
+    -- Drop all foreign key constraints
+    DECLARE @sql NVARCHAR(MAX) = N'';
+    SELECT @sql += 'ALTER TABLE ' + QUOTENAME(s.name) + '.' + QUOTENAME(t.name) + ' DROP CONSTRAINT ' + QUOTENAME(f.name) + ';'
+    FROM sys.foreign_keys AS f
+    INNER JOIN sys.tables AS t ON f.parent_object_id = t.object_id
+    INNER JOIN sys.schemas AS s ON t.schema_id = s.schema_id;
+    EXEC sp_executesql @sql;
+    
+    -- Drop all tables
+    SET @sql = N'';
+    SELECT @sql += 'DROP TABLE ' + QUOTENAME(s.name) + '.' + QUOTENAME(t.name) + ';'
+    FROM sys.tables AS t
+    INNER JOIN sys.schemas AS s ON t.schema_id = s.schema_id;
+    EXEC sp_executesql @sql;
+    
+    COMMIT TRANSACTION
+END TRY
+BEGIN CATCH
+SELECT
+    ERROR_NUMBER() AS ErrorNumber,
+    ERROR_SEVERITY() AS ErrorSeverity,
+    ERROR_STATE() AS ErrorState,
+    ERROR_PROCEDURE() AS ErrorProcedure,
+    ERROR_LINE() AS ErrorLine,
+    ERROR_MESSAGE() AS ErrorMessage
+    ROLLBACK TRANSACTION
+END CATCH
