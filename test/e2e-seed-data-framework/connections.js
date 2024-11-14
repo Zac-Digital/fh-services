ca6934c232c6051c387b5e9ae6db3a3f69fd57ad
Zac-AND-Digital <zac.king@and.digital>
2024-11-14 15:13:07 +0000

    chore: remove commented out code

--- test/e2e-seed-data-framework/connections.js
+++ test/e2e-seed-data-framework/connections.js
@@ -5,39 +5,19 @@ import '@dotenvx/dotenvx'
 export const serviceDirectoryDb = buildSequelizeConnection(process.env.CONNECTION_STRING_SERVICEDIRECTORY);
 export const referralDb = buildSequelizeConnection(process.env.CONNECTION_STRING_REFERRAL);
 
-// export const serviceDirectoryDb = new Sequelize('database', 'username', 'password', {
-//   dialect: 'mssql',
-//   dialectOptions: {
-//     connectionString: process.env.CONNECTION_STRING_SERVICEDIRECTORY
-//   }
-// });
-
-// export const serviceDirectoryDb = buildSequelizeConnection(
-//   process.env.DB_SD_HOST, process.env.DB_SD_NAME, process.env.DB_SD_USER, process.env.DB_SD_PASSWORD)
-
-// export const referralDb = buildSequelizeConnection(
-//   process.env.DB_REFERRAL_HOST, process.env.DB_REFERRAL_NAME, process.env.DB_REFERRAL_USER, process.env.DB_REFERRAL_PASSWORD)
-
 function buildSequelizeConnection(connectionString) {
-  let connectionStringParsed = parseConnectionString(connectionString);
+  const connectionStringParsed = parseConnectionString(connectionString);
 
-  let database = connectionStringParsed["initial catalog"];
-  let username = connectionStringParsed["user id"];
-  let password = connectionStringParsed["password"];
+  const database = connectionStringParsed["initial catalog"];
+  const username = connectionStringParsed["user id"];
+  const password = connectionStringParsed["password"];
+  const host = connectionStringParsed["server"];
 
   return new Sequelize(database, username, password, {
     dialect: 'mssql',
-    host: connectionStringParsed["server"]
+    host: host
   });
 }
-
-// function buildSequelizeConnection (dbHost, dbName, dbUser, dbPassword) {
-//   console.log(`Building DB connection for ${dbName} @ ${dbHost} (user ${dbUser})`)
-//   return new Sequelize(dbName, dbUser, dbPassword, {
-//     host: dbHost,
-//     dialect: 'mssql'
-//   })
-// }
 
 export async function closeConnections () {
   await serviceDirectoryDb.close()
