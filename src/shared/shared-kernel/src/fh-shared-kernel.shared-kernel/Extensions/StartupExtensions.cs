f018d317fd6e04c32c17a9f37cdf3b13adb163b1
Stuart Maskell <stuart.maskell@hippodigital.co.uk>
2024-11-22 10:03:36 +0000

    FHB-969  acceptance tests KV (#254)
    * feat: reporting pt1
    
    Added user secrets and appsettings for KV
    
    * chore: ConfigureAzureKeyVault
    
    Update the method to accept IConfigurationBuilder so that any form of builder can use it.
    
    * feat: AcceptanceTests KV
    
    Add keyVault configuration to acceptance tests
    
    * chore: tidy up some vars
    
    * chore: SD secrets and appsettings.json
    
    * chore: referral appsettings.json
    
    * docs: Update acceptance README.md
    
    Also removed the unnecessary extra method.

--- src/shared/shared-kernel/src/fh-shared-kernel.shared-kernel/Extensions/StartupExtensions.cs
+++ src/shared/shared-kernel/src/fh-shared-kernel.shared-kernel/Extensions/StartupExtensions.cs
@@ -7,18 +7,21 @@ namespace FamilyHubs.SharedKernel.Extensions;
 
 public static class StartupExtensions
 {
-    public static void ConfigureAzureKeyVault(this ConfigurationManager configuration)
+    public static IConfigurationBuilder ConfigureAzureKeyVault(this IConfigurationBuilder configuration)
     {
-        var keyVaultEndpoint = configuration["AppConfiguration:KeyVaultPrefix"];
-        var keyVaultIdentifier = configuration["AppConfiguration:KeyVaultIdentifier"];
+        var tempConfig = configuration.Build();
+        var keyVaultEndpoint = tempConfig["AppConfiguration:KeyVaultPrefix"];
+        var keyVaultIdentifier = tempConfig["AppConfiguration:KeyVaultIdentifier"];
 
         if (!string.IsNullOrEmpty(keyVaultEndpoint) && !string.IsNullOrEmpty(keyVaultIdentifier))
         {
-            configuration.AddAzureKeyVault(
+            return configuration.AddAzureKeyVault(
                 new Uri($"https://{keyVaultIdentifier}.vault.azure.net/"),
                 new DefaultAzureCredential(),
                 new PrefixKeyVaultSecretManager(keyVaultEndpoint));
         }
+
+        return configuration;
     }
 
     public sealed class PrefixKeyVaultSecretManager(string prefix) : KeyVaultSecretManager
