6a86daa47700b6a03f5bc4f9cc9d0105ce690d34
Steve Saunders <steve.saunders@education.gov.uk>
2024-12-11 10:25:29 +0000

    Remove BDDfy and just use a simple approach

--- src/service/service-directory-api/acceptance-tests/FamilyHubs.ServiceDirectory.Api.AcceptanceTests/Tests/Stories/ApiInfoTests.cs
+++ src/service/service-directory-api/acceptance-tests/FamilyHubs.ServiceDirectory.Api.AcceptanceTests/Tests/Stories/ApiInfoTests.cs
@@ -1,18 +1,21 @@
-using TestStack.BDDfy;
 using Xunit;
 using FamilyHubs.ServiceDirectory.Api.AcceptanceTests.Tests.Steps;
 
 namespace FamilyHubs.ServiceDirectory.Api.AcceptanceTests.Tests.Stories;
 
+/// <summary>
+/// As a UI service
+/// I want to be able to check the API health
+/// So that I can send calls to the API to get or manage data 
+/// </summary>
 public class ApiInfoTests
 {
     private readonly ApiInfoSteps _steps = new();
 
     [Fact]
-    public void Api_Info_Returned()
+    public async Task Api_Info_Returned()
     {
-        this.When(s => _steps.CheckTheApiInfo())
-            .Then(s => _steps.AnOkStatusCodeIsReturned())
-            .BDDfy();
+        await _steps.CheckTheApiInfo();
+        _steps.AnOkStatusCodeIsReturned();
     }
 }
--- src/service/service-directory-api/acceptance-tests/FamilyHubs.ServiceDirectory.Api.AcceptanceTests/Tests/Stories/ServiceSearchTests.cs
+++ src/service/service-directory-api/acceptance-tests/FamilyHubs.ServiceDirectory.Api.AcceptanceTests/Tests/Stories/ServiceSearchTests.cs
@@ -1,57 +1,47 @@
 using System.Net;
-using TestStack.BDDfy;
 using FamilyHubs.ServiceDirectory.Api.AcceptanceTests.Tests.Steps;
 using Xunit;
 
 namespace FamilyHubs.ServiceDirectory.Api.AcceptanceTests.Tests.Stories;
 
-// Define the story/feature being tested
-[Story(
-    AsA = "user of the metrics api",
-    IWant = "to be able to update usage information for postcode searches and filters",
-    SoThat = "I can record how search functionality is being used")]
+/// <summary>
+/// As a user of the metrics api
+/// I want to be able to update usage information for postcode searches and filters
+/// So that I can record how search functionality is being used
+/// </summary>
 public class ServiceSearchTests
 {
-    private readonly ServiceSearchSteps _steps;
-
-    public ServiceSearchTests()
+    private readonly ServiceSearchSteps _steps = new();
+    
+    [Theory]
+    [InlineData("20", "E1 2EN", "200", "1", "2")] // As a Find user
+    [InlineData("5", "E1 2EN", "401", "2", "2")] // As a Find user subsequent search
+    [InlineData("20", "E1 2EN", "500", "1", "1")] // As a Connect user
+    [InlineData("0", "E1 2EN", "400", "2", "1")] // As a Connect user subsequent search
+    public async Task Service_Search_Metrics_Endpoint_Returns_A_Status_Code_Ok(
+        string radius, 
+        string postcode, 
+        string statusCode, 
+        string searchTriggerEventId, 
+        string serviceSearchTypeId)
     {
-        //Get instances of the steps required for the test
-        _steps = new ServiceSearchSteps();
+        _steps.GivenIHaveASearchServiceRequest(radius, postcode, statusCode, searchTriggerEventId, serviceSearchTypeId);
+        await _steps.WhenISendARequest();
+        _steps.ThenExpectedStatusCodeReturned(HttpStatusCode.OK);
     }
 
-    //Add all tests that make up the story to this class.
     [Theory]
-    [InlineData("20", "E1 2EN", "200", "1", "2", HttpStatusCode.OK)] // As a Find user
-    [InlineData("5", "E1 2EN", "401", "2", "2", HttpStatusCode.OK)] // As a Find user subsequent search
-    [InlineData("20", "E1 2EN", "500", "1", "1", HttpStatusCode.OK)] // As a Connect user
-    [InlineData("0", "E1 2EN", "400", "2", "1", HttpStatusCode.OK)] // As a Connect user subsequent search
-    public void Service_Search_Metrics_Endpoint_Returns_A_Status_Code_Ok(
-        string radius, string postcode, string statusCode, string searchTriggerEventId, string serviceSearchTypeId,
-        HttpStatusCode expectedStatusCode)
+    [InlineData("15", "E1 2EN", "200", "3", "1")] // Invalid Search Trigger Event Type
+    [InlineData("15", "E1 2EN", "200", "1", "3")] // Invalid ServiceSearchTypeId
+    public async Task Service_Search_Metrics_Endpoint_Returns_Status_Code_Internal_Server_Error(
+        string radius, 
+        string postcode, 
+        string statusCode,
+        string searchTriggerEventId, 
+        string serviceSearchTypeId)
     {
-        this.Given(s =>
-                _steps.GivenIHaveASearchServiceRequest(radius, postcode, statusCode, searchTriggerEventId,
-                    serviceSearchTypeId))
-            .When(s => _steps.WhenISendARequest())
-            .Then(s => _steps.ThenExpectedStatusCodeReturned(expectedStatusCode))
-            .BDDfy();
-    }
-    
-
-    
-    //Negative Scenarios
-    [Theory]
-    [InlineData("15", "E1 2EN", "200", "3", "1", HttpStatusCode.InternalServerError)] // Invalid Search Trigger Event Type
-    [InlineData("15", "E1 2EN", "200", "1", "3", HttpStatusCode.InternalServerError)] // Invalid ServiceSearchTypeId
-    public void Service_Search_Metrics_Endpoint_Returns_Status_Code_Internal_Server_Error(string radius, string postcode, string statusCode,
-        string searchTriggerEventId, string serviceSearchTypeId, HttpStatusCode expectedStatusCode)
-    {
-        this.Given(s =>
-                _steps.GivenIHaveASearchServiceRequest(radius, postcode, statusCode, searchTriggerEventId,
-                    serviceSearchTypeId))
-            .When(s => _steps.WhenISendARequest())
-            .Then(s => _steps.ThenExpectedStatusCodeReturned(expectedStatusCode))
-            .BDDfy();
+        _steps.GivenIHaveASearchServiceRequest(radius, postcode, statusCode, searchTriggerEventId, serviceSearchTypeId);
+        await _steps.WhenISendARequest();
+        _steps.ThenExpectedStatusCodeReturned(HttpStatusCode.InternalServerError);
     }
 }
