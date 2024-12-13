490bc8807d447ba7b2dc31cf46303d950b002516
Thomas Cheyney <top_cat@thomasc.co.uk>
2024-12-13 11:15:41 +0000

    fix: Always enable Hsts and HttpsRedirection

--- src/shared/web-components/example/FamilyHubs.Example/Program.cs
+++ src/shared/web-components/example/FamilyHubs.Example/Program.cs
@@ -13,14 +13,8 @@ var app = builder.Build();
 
 app.UseFamilyHubs();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
 app.UseHttpsRedirection();
+app.UseHsts();
 app.UseStaticFiles();
 
 app.UseRouting();
--- src/ui/idam-maintenance-ui/src/FamilyHubs.Idams.Maintenance.UI/StartupExtensions.cs
+++ src/ui/idam-maintenance-ui/src/FamilyHubs.Idams.Maintenance.UI/StartupExtensions.cs
@@ -309,15 +309,8 @@ public static class StartupExtensions
 
         app.UseFamilyHubs();
 
-        // Configure the HTTP request pipeline.
-        if (!app.Environment.IsDevelopment())
-        {
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts();
-        }
-
         app.UseHttpsRedirection();
-
+        app.UseHsts();
         app.UseStaticFiles();
 
         app.UseRouting();
