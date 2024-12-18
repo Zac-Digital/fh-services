ef0f061a8a0fedffb2405c8d9269c7a5f11d8a31
Steve Saunders <steve.saunders@education.gov.uk>
2024-12-18 09:59:53 +0000

    Add strict dynamic header for scripts

--- src/shared/web-components/src/FamilyHubs.SharedKernel.Razor/Security/SecurityHeaders.cs
+++ src/shared/web-components/src/FamilyHubs.SharedKernel.Razor/Security/SecurityHeaders.cs
@@ -59,7 +59,7 @@ public static class SecurityHeaders
                         });
 
                     builder.AddScriptSrc()
-                        .UnsafeInline()
+                        .StrictDynamic()
                         .WithNonce();
 
                     builder.AddStyleSrc()
