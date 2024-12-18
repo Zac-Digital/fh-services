963e76607db5e8ed83f56f85993bd01e0fa4813c
Steve Saunders <steve.saunders@education.gov.uk>
2024-12-18 10:39:01 +0000

    chore: Add unsafe inline as well as struct dynamic for legacy browsers

--- src/shared/web-components/src/FamilyHubs.SharedKernel.Razor/Security/SecurityHeaders.cs
+++ src/shared/web-components/src/FamilyHubs.SharedKernel.Razor/Security/SecurityHeaders.cs
@@ -59,6 +59,7 @@ public static class SecurityHeaders
                         });
 
                     builder.AddScriptSrc()
+                        .UnsafeInline()
                         .StrictDynamic()
                         .WithNonce();
 
