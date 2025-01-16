2314ad045c9316dc4915dfff4db68376215ca571
Zac-AND-Digital <zac.king@and.digital>
2025-01-16 16:49:43 +0000

    feat: navigation links are feature flagged

--- src/shared/shared-kernel/src/fh-shared-kernel.shared-kernel/FeatureManagement/FeatureManagement.cs
+++
@@ -1,6 +0,0 @@
-namespace FamilyHubs.SharedKernel.FeatureManagement;
-
-public static class FeatureManagement
-{
-    public const string FeatureConnectDashboard = "FeatureConnectDashboard";
-}
--- src/ui/connect-ui/src/FamilyHubs.Referral.Web/Pages/Shared/HeaderPageModel.cs
+++ src/ui/connect-ui/src/FamilyHubs.Referral.Web/Pages/Shared/HeaderPageModel.cs
@@ -3,6 +3,7 @@ using FamilyHubs.SharedKernel.Razor.FamilyHubsUi.Options;
 using FamilyHubs.SharedKernel.Razor.Header;
 using FamilyHubs.SharedKernel.Razor.Links;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.FeatureManagement;
 
 namespace FamilyHubs.Referral.Web.Pages.Shared;
 
@@ -34,6 +35,18 @@ public class HeaderPageModel : PageModel, IFamilyHubsHeader
         FhLinkOptions[] navigationLinks,
         IFamilyHubsUiOptions familyHubsUiOptions)
     {
+        // // Since 40-50 classes derive this class, using normal dependency injection would mean passing IFeatureManager
+        // // into all their constructors as most of them call base(..) to pass through stuff to this constructor.
+        // // So I'm fetching it in here :)
+        // IFeatureManager featureManager = HttpContext.RequestServices.GetRequiredService<IFeatureManager>();
+        //
+        // if (! await featureManager.IsEnabledAsync(FeatureManagement.FeatureConnectDashboard))
+        // {
+        //     // The navigation links are loaded from the appsettings.json at startup for some reason so the easiest thing to do is
+        //     // to just chop off the "My requests" tab from the array.
+        //     return [ navigationLinks[0] ];
+        // }
+        
         string role = HttpContext.GetRole();
 
         return role is RoleTypes.VcsProfessional or RoleTypes.VcsDualRole
