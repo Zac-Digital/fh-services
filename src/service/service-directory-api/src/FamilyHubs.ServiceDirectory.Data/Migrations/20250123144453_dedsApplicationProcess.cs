ea15769dbb4c6ae5541381dd841832b4f19e83e4
Stuart Maskell <stuart.maskell@hippodigital.co.uk>
2025-01-28 09:12:55 +0000

    fix(deds): ApplicationProcess (#393)
    ApplicationProcess is a description field and could contain much more than 512 characters

---
+++ src/service/service-directory-api/src/FamilyHubs.ServiceDirectory.Data/Migrations/20250123144453_dedsApplicationProcess.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FamilyHubs.ServiceDirectory.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class dedsApplicationProcess : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ApplicationProcess",
+                schema: "deds",
+                table: "Services",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(512)",
+                oldMaxLength: 512,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ApplicationProcess",
+                schema: "deds",
+                table: "Services",
+                type: "nvarchar(512)",
+                maxLength: 512,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
