72910a6e84ebb02fb09392095c654f40dd1a15bb
Stuart.Maskell <stuart.maskell@education.gov.uk>
2025-02-06 16:32:14 +0000

    feat(deds): Add checksum field and migration

---
+++ src/service/service-directory-api/src/FamilyHubs.ServiceDirectory.Data/Migrations/20250206162130_DedsServiceChecksum.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FamilyHubs.ServiceDirectory.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class DedsServiceChecksum : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "Checksum",
+                schema: "deds",
+                table: "Services",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Checksum",
+                schema: "deds",
+                table: "Services");
+        }
+    }
+}
