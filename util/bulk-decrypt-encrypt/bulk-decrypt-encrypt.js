71568fbaa67921d81783c880d2d18501168cbc5f
Zac-AND-Digital <zac.king@and.digital>
2025-03-02 17:35:31 +0000

    wip: script

--- util/bulk-decrypt-encrypt/bulk-decrypt-encrypt.js
+++ util/bulk-decrypt-encrypt/bulk-decrypt-encrypt.js
@@ -21,6 +21,7 @@ const ENCRYPTION_KEY_INITIALISATION_VECTOR__IDAM_API = new Uint8Array(process.en
 const IDAM_DB = buildSequelizeConnection(process.env.CONNECTION_STRING_IDAM);
 const NOTIFICATION_DB = buildSequelizeConnection(process.env.CONNECTION_STRING_NOTIFICATION);
 const REFERRAL_DB = buildSequelizeConnection(process.env.CONNECTION_STRING_REFERRAL);
+const REPORT_DB = buildSequelizeConnection(process.env.CONNECTION_STRING_REPORT);
 // -- Variables --
 
 // -- Functions --
@@ -39,31 +40,33 @@ function buildSequelizeConnection(connectionString) {
 }
 
 async function checkConnections() {
-    console.log("Checking Connections are OK...");
+    console.info("Checking Connections are OK...");
 
     try {
         await IDAM_DB.authenticate();
         await NOTIFICATION_DB.authenticate();
         await REFERRAL_DB.authenticate();
+        await REPORT_DB.authenticate();
     } catch (error) {
         console.error("Unable to establish connection to the database:", error);
     }
 
-    console.log("Connections are OK!");
+    console.info("Connections are OK!");
 }
 
 async function closeConnections() {
-    console.log("Closing Database Connections...");
+    console.info("Closing Database Connections...");
 
     try {
         await IDAM_DB.close();
         await NOTIFICATION_DB.close();
         await REFERRAL_DB.close();
+        await REPORT_DB.close();
     } catch (error) {
         console.error("Unable to close connection to the database:", error);
     }
 
-    console.log("Connections Closed!");
+    console.info("Connections Closed!");
 }
 
 function decrypt(database, ciphertext) {
@@ -83,7 +86,8 @@ function decrypt(database, ciphertext) {
             KEY = DECRYPTION_KEY__NOTIFICATION_API;
             IV = DECRYPTION_KEY_INITIALISATION_VECTOR__NOTIFICATION_API;
             break;
-        case REFERRAL_DB:    
+        case REFERRAL_DB:
+        case REPORT_DB:
             KEY = DECRYPTION_KEY__REFERRAL_API
             IV = DECRYPTION_KEY_INITIALISATION_VECTOR__REFERRAL_API;
             break;
@@ -123,6 +127,7 @@ function encrypt(database, plaintext) {
             IV = ENCRYPTION_KEY_INITIALISATION_VECTOR__NOTIFICATION_API;
             break;
         case REFERRAL_DB:
+        case REPORT_DB:
             KEY = ENCRYPTION_KEY__REFERRAL_API
             IV = ENCRYPTION_KEY_INITIALISATION_VECTOR__REFERRAL_API;
             break;
@@ -140,67 +145,159 @@ function encrypt(database, plaintext) {
     return encrypted;
 }
 
-async function rotateIdamDb() {
-    const tableInfo = await IDAM_DB.query("SELECT TABLE_NAME, TABLE_SCHEMA FROM INFORMATION_SCHEMA.TABLES;");
+async function updateDatabaseWithNewEncryptedValues(DATABASE, TRANSACTION) {
+    const tableInfo = await DATABASE.query("SELECT TABLE_NAME, TABLE_SCHEMA FROM INFORMATION_SCHEMA.TABLES;", { transaction: TRANSACTION });
     
     for (const tableItem of tableInfo[0]) {
         const tableName = `[${tableItem["TABLE_SCHEMA"]}].[${tableItem["TABLE_NAME"]}]`;
         
-        const firstRow = await IDAM_DB.query(`SELECT TOP 1 * FROM ${tableName}`);
+        const rows = await DATABASE.query(`SELECT * FROM ${tableName}`, { transaction: TRANSACTION });
         
-        // Empty table
-        if (firstRow[1] === 0) continue;
-        
-        const columnInfo = await IDAM_DB.query(`SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = N'${tableItem["TABLE_NAME"]}'`);
+        if (rows[1] === 0) {
+            console.info(`${tableName} is empty, skipping!`);
+            continue;
+        }
 
-        for (const columnName of columnInfo[0]) {
-            const value = firstRow[0][0][columnName["COLUMN_NAME"]];
-            
-            const decryptedValue = decrypt(IDAM_DB, value);
-            
-            if (decryptedValue == null) continue;
-            
-            console.log(`${tableName} - ${columnName["COLUMN_NAME"]} - ${value} - ${decryptedValue}`);
+        let isIdColumn = false;
+        for (const key of Object.keys(rows[0][0])) {
+            if (key !== "Id") continue;
+            isIdColumn = true;
+            break;
+        }
+
+        if (!isIdColumn) {
+            console.info(`${tableName} does not contain an "Id" column, skipping!`)
+            continue;
         }
         
-        /*
-        TODO:
-            1. See what columns are encrypted by looking at each column of the first item in a table
-            2. SELECT (X, Y, Z) FROM [Table Name] (where X, Y, Z are encrypted columns}
-            3. For each result: Decrypt [X|Y|Z], Encrypt [X|Y|Z], Update Table
-                - ^ See below commented out code for what that looks like on a hard-coded example, but make generic
-         */
+        for (const row of rows[0]) {
+            const updateQuery = [];
+            
+            for (const key of Object.keys(row)) {
+                const value = row[key];
+                const decrypted = decrypt(DATABASE, value);
+                if (decrypted == null) continue;
+                const encrypted = encrypt(DATABASE, decrypted);
+                updateQuery.push(`[${key}] = '${encrypted}'`);
+            }
+            
+            if (updateQuery.length === 0) continue;
+            
+            await DATABASE.query(`UPDATE ${tableName} SET ${updateQuery.join(", ")} WHERE [Id] = ${row["Id"]}`, { transaction: TRANSACTION });
+        }
     }
-    
-    // const accountClaims = await IDAM_DB.query("SELECT [Id], [CreatedBy], [LastModifiedBy] FROM [dbo].[AccountClaims]");
-    //
-    // for (const accountClaim of accountClaims[0]) {
-    //     const createdBy_Decrypted = decrypt(IDAM_DB, accountClaim["CreatedBy"]);
-    //     const lastModifiedBy_Decrypted = decrypt(IDAM_DB, accountClaim["LastModifiedBy"]);
-    //    
-    //     const createdBy_Encrypted = encrypt(IDAM_DB, createdBy_Decrypted);
-    //     const lastModifiedBy_Encrypted = encrypt(IDAM_DB, lastModifiedBy_Decrypted);
-    //    
-    //     await IDAM_DB.query(`UPDATE [dbo].[AccountClaims] SET [CreatedBy] = '${createdBy_Encrypted}', [LastModifiedBy] = '${lastModifiedBy_Encrypted}' WHERE [Id] = ${accountClaim["Id"]}`);
-    // }
-    //
-    // const accounts = await IDAM_DB.query("SELECT [Id], [Name], [Email]");
 }
 // -- Functions --
 
 // -- Entry Point --
 export async function main() {
-    await checkConnections();
 
-    await rotateIdamDb();
+    /**
+     * Since we have multiple independent databases, we need to have a transaction defined for each.
+     * 
+     * However, should any table in any database fail to update - we want to reverse the entire operation across
+     * all databases.
+     * 
+     * Therefore, to give the equivalent functionality, any exception rolls back all databases, and changes are only
+     * committed if each try-catch block is successful (aka no rollbacks).
+     */
     
-    await closeConnections();
+    const transaction_IdAM = await IDAM_DB.transaction();
+    const transaction_Notification = await NOTIFICATION_DB.transaction();
+    const transaction_Referral = await REFERRAL_DB.transaction();
+    const transaction_Report = await REPORT_DB.transaction();
+    
+    console.info("\n----------------------------------------------------------------\n");
+    
+    try {
+        console.info("Updating the IdAM Database...");
+        await updateDatabaseWithNewEncryptedValues(IDAM_DB, transaction_IdAM);
+        console.info("IdAM Database Updated!");
+    } catch (error) {
+        console.error(error);
+        await transaction_IdAM.rollback();
+        await transaction_Notification.rollback();
+        await transaction_Referral.rollback();
+        await transaction_Report.rollback();
+        return;
+    }
+
+    console.info("\n----------------------------------------------------------------\n");
+
+    try {
+        console.info("Updating the Notification Database...");
+        await updateDatabaseWithNewEncryptedValues(NOTIFICATION_DB, transaction_Notification);
+        console.info("Notification Database Updated!");
+    } catch (error) {
+        console.error(error);
+        await transaction_IdAM.rollback();
+        await transaction_Notification.rollback();
+        await transaction_Referral.rollback();
+        await transaction_Report.rollback();
+        return;
+    }
+
+    console.info("\n----------------------------------------------------------------\n");
+
+    try {
+        console.info("Updating the Referral Database...");
+        await updateDatabaseWithNewEncryptedValues(REFERRAL_DB, transaction_Referral);
+        console.info("Referral Database Updated!");
+    } catch (error) {
+        console.error(error);
+        await transaction_IdAM.rollback();
+        await transaction_Notification.rollback();
+        await transaction_Referral.rollback();
+        await transaction_Report.rollback();
+        return;
+    }
+
+    console.info("\n----------------------------------------------------------------\n");
+    
+    try {
+        console.info("Updating the Report Database...");
+        await updateDatabaseWithNewEncryptedValues(REPORT_DB, transaction_Report);
+        console.info("Report Database Updated!");
+    } catch (error) {
+        console.error(error);
+        await transaction_IdAM.rollback();
+        await transaction_Notification.rollback();
+        await transaction_Referral.rollback();
+        await transaction_Report.rollback();
+        return;
+    }
+
+    console.info("\n----------------------------------------------------------------\n");
+
+    console.info("All databases have been successfully updated, committing the transactions...");
+    await transaction_IdAM.commit();
+    await transaction_Notification.commit();
+    await transaction_Referral.commit();
+    await transaction_Report.commit();
+    console.info("Transactions committed!");
 }
 
+/**
+ * TODO:
+ *  - Everything* seems to be working including transactions - so if any table in any Db fails, EVERYTHING is rolled back
+ *  - Could be tidied up a bit, maybe split out into more files & functions - have just got the functionality working for now
+ *  - So far I have been testing with the same keys for both decrypting and encrypting. For each environment as this gets
+ *    executed, new keys will need to be generated from each respective IdAM Maintenance UI and copied into your .env file
+ *  - ^^^^^^^ Make sure to copy the old keys first into the decryption sections!!
+ *  - Will probably be best to get a couple of people into a huddle and do it together when it comes to running it against each live environment
+ *  - * Report Db is weird. Not sure yet if it'll need to be treated differently to the others.
+ */
+
+let success = true;
+
 try {
+    await checkConnections();
     await main();
 } catch (error) {
-    console.log(error);
-    process.exit(1)
+    console.error(error);
+    success = false;
+} finally {
+    await closeConnections();
+    if (!success) process.exit(1);
 }
 // -- Entry Point --
